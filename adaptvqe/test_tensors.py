1e71c79e16d5524a7ef0afda4c492642a38644bb
Benjamin DalFavero <bdalfavero@gmail.com>
2025-12-02 10:26:23 -0500

    Merged tests into a single file.

--- adaptvqe/test_tensors.py
+++ adaptvqe/test_tensors.py
@@ -1,10 +1,23 @@
 import unittest
 import numpy as np
 import openfermion as of
-from quimb.tensor.tensor_1d import MatrixProductOperator
+from quimb.tensor.tensor_1d import MatrixProductOperator, MatrixProductState
 from adaptvqe.pools import FullPauliPool, ImplementationType
 from adaptvqe.tensor_helpers import computational_basis_mps
 from adaptvqe.matrix_tools import ket_to_vector
+
+class TestMPSConstruction(unittest.TestCase):
+
+    def test_binary_cb_state(self):
+        qnums = [0, 1, 1]
+        generated_mps = computational_basis_mps(qnums)
+        mps_dense = generated_mps.to_dense()
+        # Construct the state exactly.
+        psi = np.zeros(2 ** 3, dtype=complex)
+        psi[3] = 1.
+        psi = psi.reshape(mps_dense.shape)
+        assert np.allclose(psi, mps_dense)
+
 
 class TestPauliPool(unittest.TestCase):
 
