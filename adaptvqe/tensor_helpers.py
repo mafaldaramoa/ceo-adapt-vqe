b7fc1773f8096ab0a2dc697150d7fbb5803de51f
Benjamin DalFavero <bdalfavero@gmail.com>
2025-12-04 16:15:57 -0500

    Added function to convert an MPS to a vector.

--- adaptvqe/tensor_helpers.py
+++ adaptvqe/tensor_helpers.py
@@ -83,3 +83,21 @@ def computational_basis_mps(qnums: List[int], d: int=2, **kwargs) -> MatrixProdu
             this_tensor[0, qnum, 0] = 1.
         tensors.append(this_tensor.copy())
     return MatrixProductState(tensors, shape='lpr', **kwargs)
+
+
+def mps_to_vector(mps: MatrixProductState) -> np.ndarray:
+    """Convert an MPS into a normal vector. This assumes each index is a string
+    followed by a number, e.g. three indices 'k0, k1, k2'."""
+
+    def _idx_to_int(idx: str) -> int:
+        digits = [c for c in idx if c in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']]
+        if len(digits) == 0:
+            raise ValueError(f"Index {str} has no digits in it.")
+        return int(''.join(digits))
+    
+    # Contract the MPS into a tensor, then sort the indices. Convert that to a vector.
+    contracted_tensor = mps.contract()
+    sorted_inds = sorted(contracted_tensor.inds, key=_idx_to_int)
+    contracted_tensor.transpose(*sorted_inds, inplace=True)
+    tensor_data = contracted_tensor.data
+    return tensor_data.reshape((tensor_data.size,))
