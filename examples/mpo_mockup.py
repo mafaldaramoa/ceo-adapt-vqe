7599d98de5c8d155ce63633800307fb4b469a2a7
Benjamin DalFavero <bdalfavero@gmail.com>
2025-12-11 10:44:49 -0800

    Printed tensors to show that only the first MPO is left in the sum.

--- examples/mpo_mockup.py
+++ examples/mpo_mockup.py
@@ -5,10 +5,14 @@ from quimb.tensor.tensor_1d import MatrixProductOperator
 def fill_fun(shape: Tuple[int], fill_val: float=1.) -> np.ndarray:
     return fill_val * np.ones(shape)
 
+chi = 5
 for L in range(1, 4):
-    for chi in range(1, 5):
-        mpo1 = MatrixProductOperator.from_fill_fn(lambda shape: fill_fun(shape, 1), L, chi)
-        mpo2 = MatrixProductOperator.from_fill_fn(lambda shape: fill_fun(shape, -3), L, chi)
-        mpo_sum = mpo1 + mpo2
-        matrix_sum = mpo1.to_dense() + mpo2.to_dense()
-        print(f"L={L} chi={chi}:", np.allclose(mpo_sum.to_dense(), matrix_sum))
+    mpo1 = MatrixProductOperator.from_fill_fn(lambda shape: fill_fun(shape, 1), L, chi)
+    mpo2 = MatrixProductOperator.from_fill_fn(lambda shape: fill_fun(shape, -3), L, chi)
+    mpo_sum = mpo1 + mpo2
+    matrix_sum = mpo1.to_dense() + mpo2.to_dense()
+    print(f"L={L}:", np.allclose(mpo_sum.to_dense(), matrix_sum))
+    if L == 1:
+        print("mpo1=\n", mpo1.tensors[0].data)
+        print("mpo2=\n", mpo2.tensors[0].data)
+        print("sum=\n", mpo_sum.tensors[0].data)
