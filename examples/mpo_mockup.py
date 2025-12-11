3782b0cb83f7d0e6ef4216efcc8c2f631dfe2a7f
Benjamin DalFavero <bdalfavero@gmail.com>
2025-12-11 10:40:25 -0800

    Looped over lengths to demonstrate that L=1 is an edge case.

--- examples/mpo_mockup.py
+++ examples/mpo_mockup.py
@@ -5,11 +5,10 @@ from quimb.tensor.tensor_1d import MatrixProductOperator
 def fill_fun(shape: Tuple[int], fill_val: float=1.) -> np.ndarray:
     return fill_val * np.ones(shape)
 
-L=1
-chi=2
-mpo1 = MatrixProductOperator.from_fill_fn(lambda shape: fill_fun(shape, 1), L, chi)
-mpo2 = MatrixProductOperator.from_fill_fn(lambda shape: fill_fun(shape, -3), L, chi)
-mpo_sum = mpo1 + mpo2
-matrix_sum = mpo1.to_dense() + mpo2.to_dense()
-
-print(np.allclose(mpo_sum.to_dense(), matrix_sum))
+for L in range(1, 4):
+    for chi in range(1, 5):
+        mpo1 = MatrixProductOperator.from_fill_fn(lambda shape: fill_fun(shape, 1), L, chi)
+        mpo2 = MatrixProductOperator.from_fill_fn(lambda shape: fill_fun(shape, -3), L, chi)
+        mpo_sum = mpo1 + mpo2
+        matrix_sum = mpo1.to_dense() + mpo2.to_dense()
+        print(f"L={L} chi={chi}:", np.allclose(mpo_sum.to_dense(), matrix_sum))
