79cc0c578c961cd587172cef942b73a1e7571e1d
Benjamin DalFavero <bdalfavero@gmail.com>
2026-01-08 15:22:19 -0500

    Added timing and CLI argument for maximum bond dimension.

--- examples/tn_large_molecule.py
+++ examples/tn_large_molecule.py
@@ -1,9 +1,16 @@
+import argparse
+from time import perf_counter_ns
 import h5py
 import numpy as np
 from quimb.tensor.tensor_dmrg import DMRG
 from adaptvqe.molecules import create_h7
 from adaptvqe.pools import DVE_CEO
 from adaptvqe.algorithms.adapt_vqe import TensorNetAdapt
+
+parser = argparse.ArgumentParser()
+parser.add_argument("max_bond", type=int, help="Maximum bond dimension of the MPS.")
+args = parser.parse_args()
+mps_max_bond = args.max_bond
 
 r = 1.5
 molecule = create_h7(r)
@@ -18,7 +25,7 @@ my_adapt = TensorNetAdapt(
     verbose=True,
     threshold=0.1,
     max_mpo_bond=100,
-    max_mps_bond = 20
+    max_mps_bond=mps_max_bond
 )
 my_adapt.initialize()
 
@@ -33,13 +40,19 @@ dmrg_energy = dmrg.energy
 print(f"Got energy {dmrg_energy}")
 
 energies = []
+times = []
 num_iter = 15
 energies.append(my_adapt.energy)
 for _ in range(num_iter):
+    start_time = perf_counter_ns()
     my_adapt.run_iteration()
+    end_time = perf_counter_ns()
+    elapsed_time = float(abs(end_time - start_time))
     energies.append(my_adapt.energy)
+    times.append(elapsed_time)
 
 f = h5py.File("h7_out.hdf5", "w")
 f.create_dataset("dmrg_energy", data=dmrg_energy)
 f.create_dataset("energies", data=np.array(energies))
+f.create_dataset("times", data=np.array(times))
 f.close()
